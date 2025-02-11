297e6bc6c42e1a875a77c21cffb2bcdf97dca5a7
chinmoybuzz <chinmoybuzz@gmail.com>
2025-02-11 12:07:16 +0530

    nav bar name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div className="container">
         <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">
           {" "}
-          React Ecommerce
+          Bucket
         </NavLink>
         <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
